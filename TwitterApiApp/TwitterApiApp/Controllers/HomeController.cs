000d72e599067312da0b3d31a2746d9953f62717
abegail cruz <daenerystargaryen03@gmail.com>
2017-10-24 23:42:57 -0500

    added tweet functionality

---
+++ TwitterApiApp/TwitterApiApp/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Tweetinvi;
+using Tweetinvi.Models;
+using TwitterApiApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tweetinvi.Credentials.Models;
+
+namespace TwitterApiApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TwitterAuth()
+        {
+            var appCreds = new ConsumerCredentials(Credentials.CONSUMER_KEY, Credentials.CONSUMER_SECRET);
+            var redirectURL = "http://" + Request.Url.Authority + "/Home/ValidateTwitterAuth";
+            var authenticationContext = AuthFlow.InitAuthentication(appCreds, redirectURL);
+
+            return new RedirectResult(authenticationContext.AuthorizationURL);
+        }
+
+        public ActionResult ValidateTwitterAuth()
+        {
+            var token = new AuthenticationToken()
+            {
+                AuthorizationKey = Credentials.ACCESS_TOKEN,
+                AuthorizationSecret = Credentials.ACCESS_TOKEN_SECRET,
+                ConsumerCredentials = new ConsumerCredentials(Credentials.CONSUMER_KEY, Credentials.CONSUMER_SECRET)
+            };
+
+            // And then instead of passing the AuthenticationContext, just pass the AuthenticationToken
+            var verifierCode = Request.Params.Get("oauth_verifier");
+
+            if (verifierCode != null)
+            {
+                var userCreds = AuthFlow.CreateCredentialsFromVerifierCode(verifierCode, token);
+                var user = Tweetinvi.User.GetAuthenticatedUser(userCreds);
+
+                ViewBag.User = user;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ TwitterApiApp/TwitterApiApp/Models/Credentials.cs
@@ -0,0 +1,22 @@
+﻿using Tweetinvi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace TwitterApiApp.Models
+{
+    public class Credentials
+    {
+        public static string CONSUMER_KEY = ConfigurationManager.AppSettings["twitterConsumerKey"];
+        public static string CONSUMER_SECRET = ConfigurationManager.AppSettings["twitterConsumerSecret"];
+        public static string ACCESS_TOKEN = ConfigurationManager.AppSettings["twitterAccessToken"];
+        public static string ACCESS_TOKEN_SECRET = ConfigurationManager.AppSettings["twitterAccessTokenSecret"];
+
+        public static ITwitterCredentials GenerateCredentials()
+        {
+            return new TwitterCredentials(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+        }
+    }
+}
